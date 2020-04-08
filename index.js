4ae48a8f6eb997f8bdd770b857d7afba8fe34f54
Sahana-Lawrence <sahana.lawrence@mths.ca>
2020-04-08 15:28:18 +0000

    updated files

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 // JavaScript File
 
-// variables assigned to calculate the area of a square or rectangle
+// variables assigned to calculate the area of a trapezoid
 function Area(){
-var base = document.getElementById("base");
+var base1 = document.getElementById("base1");
+var base2 = document.getElementById("base2")
 var height = document.getElementById("height");
-var basevalue = base.value;
+var base1value = base1.value;
+var base2value = base2.value;
 var heightvalue = height.value;
-var result = (basevalue*heightvalue);
+var result = ((base1value+base2value)*(heightvalue/2));
 var divresult = document.getElementById("divresult");
 divresult.innerHTML = result;
 }
