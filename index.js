463b410e24a6c165cad32b7ed9fdf7991bc43bf3
Sahana-Lawrence <sahana.lawrence@mths.ca>
2020-04-13 19:34:30 +0000

    updated fiels

--- index.js
+++ index.js
@@ -5,9 +5,9 @@ function Area(){
 var base1 = document.getElementById("base1");
 var base2 = document.getElementById("base2")
 var height = document.getElementById("height");
-var base1value = base1.value;
-var base2value = base2.value;
-var heightvalue = height.value;
+var base1value = +base1.value;
+var base2value = +base2.value;
+var heightvalue = +height.value;
 var result = ((base1value+base2value)*(heightvalue/2));
 var divresult = document.getElementById("divresult");
 divresult.innerHTML = result;
